30b885e6943226079c42cd635b3e3e3c5098bd03
Caitlin McEleney <caitlinmceleney@yahoo.com>
2020-05-05 18:12:40 -0400

    Get all pet information for the pet page

--- app/src/main/java/com/example/androidfinalproject/Controllers/PetPage.kt
+++ app/src/main/java/com/example/androidfinalproject/Controllers/PetPage.kt
@@ -3,42 +3,36 @@ package com.example.androidfinalproject.Controllers
 import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidfinalproject.Models.Pet
 import com.example.androidfinalproject.R
+import kotlinx.android.synthetic.main.activity_pet_page.*
+import java.sql.Types.NULL
 import java.util.*
 
 
 class PetPage : AppCompatActivity() {
 var username = ""
+var thisPet = Pet("", "NULL", "NULL", 0 , 0,0)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pet_page)
         username = intent.getStringExtra("Username")
-        var myPetAsync = getPetAsync()
-        myPetAsync.execute(username)
+        val myDb= PetDatabaseManager(applicationContext)
+        myDb.writableDatabase
+        myDb.getPet(username)
+        thisPet = myDb.getPet(username)
 
 
-        val period: Long = 0
-        Timer().schedule(object : TimerTask() {
-            override fun run() { // do your task here
-
-            }
-        }, 0, period)
+//        val period: Long = 0
+//        Timer().schedule(object : TimerTask() {
+//            override fun run() { // do your task here
+//
+//            }
+//        }, 0, period)
     }
 
-    inner class getPetAsync: AsyncTask<String, Unit?, Unit?>() {
 
-        override fun onPostExecute(result: Unit?) {
-            super.onPostExecute(result)
-            //set up pet info here
-        }
-
-        override fun doInBackground(vararg params: String) {
-            val myDb= PetDatabaseManager(applicationContext)
-            myDb.writableDatabase
-            myDb.getPet(params[0])
-            return
-        }
     }
-}
+
